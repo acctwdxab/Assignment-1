f9c532e9b8b9af2118c771e8edb97d569577c5c4
acctwdxab <71532324+acctwdxab@users.noreply.github.com>
2020-09-27 01:13:17 -0700

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,7 +1,7 @@
+# project-1
 # Author: Dan Wu
 # Date: 9/26/2020
 # Description: Asks the user for their favorite animal and then
 #              prints out that that is their favorite animal
-
-fave_animal = "dog"
+fave_animal = input("Please enter your favorite animal: ")
 print("Your favorite animal is the " + fave_animal + ".")
